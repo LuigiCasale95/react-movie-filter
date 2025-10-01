4c0f3c2716abdb78eb7d10c1df95c261875006b7
LuigiCasale95 <luigicasale95@gmail.com>
2025-10-01 14:07:20 +0200

    Creazione base per esercizio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MyMain from './MyMain'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    <MyMain></MyMain>
+    </>
+  )
+}
+
+export default App
---
+++ src/MyMain.jsx
@@ -0,0 +1,16 @@
+
+const Film = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
+
+export default function MyMain() {
+
+    return (
+        <div>CIAO</div>
+    )
+}
